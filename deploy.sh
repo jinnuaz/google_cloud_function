a2b071bbe16b558a8856b3e23232ac1c244df934
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-08-18 11:15:23 -0700

    Altering bucket name

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 gcloud functions deploy gcs-file-processor \
   --gen2 \
   --runtime=python311 \
-  --region=us \
+  --region=us-central1 \
   --source=. \
   --entry-point=process_file \
   --trigger-event-filters="type=google.cloud.storage.object.v1.finalized" \
