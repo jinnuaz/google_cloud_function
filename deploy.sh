957241a55f371482a33642533c12b2add6005798
myWorkspace537 <shrutifrancis.ai@gmail.com>
2026-08-18 11:25:51 -0700

    Altering bucket name

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ gcloud functions deploy gcs-file-processor \
   --gen2 \
   --runtime=python311 \
   --region=us-central1 \
-  --source=google_cloud_function/ \
+  --source=/home/jinnuaz/google_cloud_function/ \
   --entry-point=process_file \
   --trigger-event-filters="type=google.cloud.storage.object.v1.finalized" \
   --trigger-event-filters="bucket=urbancart-dw-landing-2026"
